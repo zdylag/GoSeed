e34af0c463b1aea9ed853fc2de2f9a914c08edb6
Zach <zdylag@gmail.com>
2022-05-28 23:19:43 +0000

    go main: simplify template go files
    This removes the unneeded main.go and main_test.go files
    and replaces them with a single file intended to be deleted
    when creating a new library from this template.

---
+++ DELETETHIS.go
@@ -0,0 +1,4 @@
+// Package deletethis is a placeholder. This file should be deleted when initializing a new repo from this template.
+package deletethis
+
+func deletable() {}
---
+++ DELETETHIS_test.go
@@ -0,0 +1,8 @@
+// Package deletethis is a placeholder. This file should be deleted when initializing a new repo from this template.
+package deletethis
+
+import "testing"
+
+func Test(t *testing.T) {
+	deletable()
+}
